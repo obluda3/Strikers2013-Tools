09a7109ea83eddca68703adbc4edffe2697470ae
obluda3 <yoshivert992@gmail.com>
2021-01-13 22:07:17 +0100

    Text exporting

--- CopaEditor/Utils/TextFile.cs
+++ CopaEditor/Utils/TextFile.cs
@@ -12,12 +12,57 @@ namespace CopaEditor.Utils
     {
         public string fileName;
         private int unkCount;
-        private uint fileType, sectNumber, offsetPointer,entryNumber,sectLength; // 14, 35 etc...
+        private uint fileType, sectNumber, offsetPointer, entryNumber, sectLength; // 14, 35 etc...
         private uint[] pointers;
-        private Encoding sjis = Encoding.GetEncoding("sjis", new EncoderExceptionFallback(),new DecoderExceptionFallback());
+        private byte[] unk1, unk2, unk3;
+        private Encoding sjis = Encoding.GetEncoding("sjis", new EncoderExceptionFallback(), new DecoderExceptionFallback());
         private Encoding utf8 = Encoding.GetEncoding("utf-8", new EncoderExceptionFallback(), new DecoderExceptionFallback());
 
-        public void ExportText(string input, string output) 
+
+        public void ExportText(string input, string output)
+        {
+            parseTextFile(input);
+            var file = File.Open(input, FileMode.Open);
+
+            using (var ber = new BeBinaryReader(file, Encoding.GetEncoding("sjis")))
+            {
+                var textFile = File.Open(output, FileMode.Create);
+                using (var txtFile = new StreamWriter(textFile))
+                {
+                    for (var i = 0; i < pointers.Length; i++)
+                    {
+                        var length = 0;
+                        if (i + 1 == pointers.Length)
+                            length = 4;
+                        else if (pointers[i] == 0)
+                            length = 0;
+                        else
+                        {
+                            length = (int)pointers[i + 1] - (int)pointers[i];
+                            var j = i + 1;
+                            while (length < 0) // Takes into account blank pointers
+                            {
+                                length = (int)pointers[j] - (int)pointers[i];
+                                j++;
+                                if (j > pointers.Length - 1) { length = 4; break; }
+                            }
+                        }
+
+                        ber.BaseStream.Position = pointers[i]
+                        var entry = ber.ReadBytes(length);
+                        var entryString = "";
+                        entryString = sjis.GetString(entry);
+                        entryString = entryString.Replace("\0", string.Empty);
+                        txtFile.WriteLine(entryString);
+                    }
+                }
+
+            }
+        }
+
+        public void ImportText(string input, string orig) { }
+
+        private void parseTextFile(string input)
         {
             var file = File.Open(input, FileMode.Open);
             using (var ber = new BeBinaryReader(file, Encoding.GetEncoding("sjis")))
@@ -39,12 +84,15 @@ namespace CopaEditor.Utils
                         unkCount = 1;
                         break;
                 }
-                ber.BaseStream.Position += unkCount * 4;
+                var unk1Length = unkCount * 4;
+                unk1 = ber.ReadBytes(unk1Length);
 
                 // Gets the the position of the first pointer and the number of entries in the text files
                 offsetPointer = ber.ReadUInt32();
                 entryNumber = ber.ReadUInt32();
-                ber.BaseStream.Position = offsetPointer;
+                
+                var unk2Length = (int)offsetPointer - (int)ber.BaseStream.Position;
+                unk2 = ber.ReadBytes(unk2Length);
 
                 pointers = new uint[entryNumber];
                 for (var i = 0; i < entryNumber; i++)
@@ -52,47 +100,10 @@ namespace CopaEditor.Utils
                     pointers[i] = ber.ReadUInt32();
                 }
 
-                var textFile = File.Open(output, FileMode.Create);
-                using (var txtFile = new StreamWriter(textFile))
-                {
-                    for (var i = 0; i < pointers.Length; i++)
-                    {
-                        var length = 0;
-                        if (i+1==pointers.Length)
-                            length = 4;
-                        else if (pointers[i] == 0)
-                            length = 0;
-                        else
-                        {
-                            length = (int)pointers[i + 1] - (int)pointers[i];
-                            var j = i+1;
-                            while (length < 0) // Takes into account blank pointers
-                            {
-                                length = (int)pointers[j] - (int)pointers[i];
-                                j++;
-                                if (j > pointers.Length - 1) { length = 4; break; }
-                            }
-                        }
-                        ber.BaseStream.Position = pointers[i];
-                        var entry = ber.ReadBytes(length);
-                        Console.WriteLine(ber.BaseStream.Position);
-                        var entryString = "";
-                        try
-                        {
-                            entryString = sjis.GetString(entry);
-
-                        }
-                        catch(EncoderFallbackException e)
-                        {
-                            Console.WriteLine(e);
-                        }
-                        entryString = entryString.Replace("\0", string.Empty);
-                        txtFile.WriteLine(entryString);
-                    }
-                }
-
+                ber.BaseStream.Position = pointers[entryNumber - 1] + 4; // After the text
+                var unk3Length = (int)ber.BaseStream.Length - (int)ber.BaseStream.Position;
+                unk3 = ber.ReadBytes(unk3Length);
             }
         }
-        public void ImportText(string input, string orig) { }
     }
 }
