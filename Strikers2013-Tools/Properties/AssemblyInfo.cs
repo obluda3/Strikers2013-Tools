90d182b6add19729c3f48364b6afe92e7085e1a1
obluda3 <yoshivert992@gmail.com>
2021-05-07 11:46:37 +0200

    Updates assemblyInfo

--- Strikers2013-Tools/Properties/AssemblyInfo.cs
+++ Strikers2013-Tools/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // Les informations générales relatives à un assembly dépendent de
 // l'ensemble d'attributs suivant. Changez les valeurs de ces attributs pour modifier les informations
 // associées à un assembly.
-[assembly: AssemblyTitle("CopaEditor")]
+[assembly: AssemblyTitle("Strikers2013-Tools")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("CopaEditor")]
+[assembly: AssemblyProduct("Strikers2013-Tools")]
 [assembly: AssemblyCopyright("Copyright ©  2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
